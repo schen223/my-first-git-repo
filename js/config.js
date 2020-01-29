40caa83dc8a52e122e806fffc135127b04e2b3fc
schen223 <shanechen13@gmail.com>
2020-01-29 01:23:37 +0000

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_sVh4vLJdT', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1rk20p5c6nster2vs0o5ifu3du', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
